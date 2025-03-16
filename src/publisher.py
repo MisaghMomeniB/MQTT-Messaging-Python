4a65add9da24eeed9286fbb18b09d7719ccaa3f5
MisaghMomeniB <jenabemisagh@gmail.com>
2025-03-16 18:02:27 +0330

    comments

--- src/publisher.py
+++ src/publisher.py
@@ -1,13 +1,19 @@
+# Importing the paho.mqtt library to interact with the MQTT protocol
 import paho.mqtt.client as mqtt
 
-BROKER = "broker.hivemq.com"
-PORT = 1883
-TOPIC = "test/topic"
+# Defining the broker address, port, and topic
+BROKER = "broker.hivemq.com"  # Broker address
+PORT = 1883  # Port for connection (default MQTT port)
+TOPIC = "test/topic"  # The topic to which the message will be published
 
+# Creating an MQTT Client object to establish communication
 client = mqtt.Client()
 
+# Connecting to the broker using the address, port, and timeout duration
 client.connect(BROKER, PORT, 60)
 
+# Publishing the message "Hello, Im Misagh" to the defined topic
 client.publish(TOPIC, "Hello, Im Misagh")
 
+# Disconnecting from the broker after the message is sent
 client.disconnect()
